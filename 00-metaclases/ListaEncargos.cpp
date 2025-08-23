12626bb61dc7a7b1f32dfb862065c69ae22e7912
aarón montoya-moraga <montoyamoraga@gmail.com>
2025-08-23 19:34:44 -0400

    agregar ListaEncargos

---
+++ 00-metaclases/ListaEncargos.cpp
@@ -0,0 +1,46 @@
+#include "ListaEncargos.h"
+
+ListaEncargos::ListaEncargos()
+{
+    ListaEncargos::lista.push_back(
+        Encargo(
+            1,
+            "01a",
+            "investigar sobre ada lovelace"));
+
+    ListaEncargos::lista.push_back(
+        Encargo(
+            2,
+            "01a",
+            "investigar sobre alan turing"));
+
+    ListaEncargos::lista.push_back(
+        Encargo(
+            3,
+            "01b",
+            "programar y equivocarse 3 veces, documentar textualmente"));
+
+    ListaEncargos::lista.push_back(
+        Encargo(
+            4,
+            "01b",
+            "programar y equivocarse 3 veces, documentar visualmente"));
+
+    ListaEncargos::lista.push_back(
+        Encargo(
+            5,
+            "02a",
+            "ponerse al día con sus apuntes en github y documentar aprendizajes y dudas"));
+
+    ListaEncargos::lista.push_back(
+        Encargo(
+            6,
+            "03a",
+            "ponerse al día con sus apuntes en github y documentar aprendizajes y dudas"));
+
+    ListaEncargos::lista.push_back(
+        Encargo(
+            6,
+            "03b",
+            "documentar aprendizajes y dificultades del trabajo con pantallas y displas SSD1306 con simulador Wokwi"));
+}
