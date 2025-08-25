9abe16f505449eb7dab1fba7f59a98b01aa12e50
Mateo Arce <163034603+matbutom@users.noreply.github.com>
2025-08-25 13:22:19 -0400

    ejemplos clase viernes sesion 03b

---
+++ 00-docentes/sesion-03b/ejemploTextoScroll02/ejemploTextoScroll02.ino
@@ -0,0 +1,75 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+Adafruit_SSD1306 pantallita(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+int contador = 0;
+
+String linea0;
+String linea1;
+String linea2;
+
+
+void setup() {
+  if(!pantallita.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("No se encontró la pantalla SSD1306"));
+    for(;;);
+  }
+  pantallita.clearDisplay();
+  pantallita.setTextColor(SSD1306_WHITE);
+
+  linea0 = "respuesta del oraculo";
+  linea1 = "hagas lo que hagas";
+  linea2 = "te arrepentiras";
+}
+
+void loop() {
+  pantallita.clearDisplay(); // Limpiamos toda la pantalla
+  
+  testscrolltext(linea0);
+  testscrolltext(linea1);
+  testscrolltext(linea2);
+  
+  pantallita.display(); // Actualiza la pantalla
+
+
+}
+
+
+// void testscrolltext(void) {
+  // cambiar la linea anterior
+  // para agregar parametro String
+  // de input
+  void testscrolltext(String texto) {
+  pantallita.clearDisplay();
+
+  pantallita.setTextSize(2); // Draw 2X-scale text
+  pantallita.setTextColor(SSD1306_WHITE);
+  pantallita.setCursor(10, 0);
+  // pantallita.println(F("scroll"));
+  // mod que le hice yo
+  pantallita.println(texto);
+
+  pantallita.display();      // Show initial text
+  delay(100);
+
+  // Scroll in various directions, pausing in-between:
+  pantallita.startscrollright(0x00, 0x0F);
+  delay(2000);
+  pantallita.stopscroll();
+  delay(1000);
+  pantallita.startscrollleft(0x00, 0x0F);
+  delay(2000);
+  pantallita.stopscroll();
+  delay(1000);
+  pantallita.startscrolldiagright(0x00, 0x07);
+  delay(2000);
+  pantallita.startscrolldiagleft(0x00, 0x07);
+  delay(2000);
+  pantallita.stopscroll();
+  delay(1000);
+}
