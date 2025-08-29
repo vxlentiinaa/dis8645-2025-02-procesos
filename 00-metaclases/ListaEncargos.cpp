f28ab27b355677af78124680141816049dd7429d
aarón montoya-moraga <montoyamoraga@gmail.com>
2025-08-29 11:57:00 -0400

    agregar encargo 8

--- 00-metaclases/ListaEncargos.cpp
+++ 00-metaclases/ListaEncargos.cpp
@@ -43,4 +43,11 @@ ListaEncargos::ListaEncargos()
             7,
             "03b",
             "documentar aprendizajes y dificultades del trabajo con pantallas y displays SSD1306 con simulador Wokwi"));
+
+    ListaEncargos::lista.push_back(
+        Encargo(
+            8,
+            "4a",
+            "investigar la poesía y el trabajo de las poetas computacionales Allison Parrish y Nick Montfort"));
+    "));
 }
