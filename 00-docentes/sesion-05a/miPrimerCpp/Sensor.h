9ab71be012dfbd94a53cefa43bacd9078ee3c82e
aarón montoya-moraga <montoyamoraga@gmail.com>
2025-09-02 11:58:57 -0400

    subir

--- 00-docentes/sesion-05a/miPrimerCpp/Sensor.h
+++ 00-docentes/sesion-05a/miPrimerCpp/Sensor.h
@@ -1,11 +1,32 @@
 #ifndef SENSOR_H
 #define SENSOR_H
 
+#include <Arduino.h>
+
 class Sensor {
-  
+
 public:
   // constructor
   Sensor();
+
+
+  int patita;
+
+  int valorActual;
+  int valorMinimo;
+  int valorMaximo;
+
+  int valorMapeado;
+  int rangoMapeadoMin;
+  int rangoMapeadoMax;
+
+  String nombre;
+
+
+
+
+
+
 };
 
 #endif
