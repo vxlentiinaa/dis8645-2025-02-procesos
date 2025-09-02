09a0b58c802beed127c6cdfde4014eb09ff57504
aarón montoya-moraga <montoyamoraga@gmail.com>
2025-09-02 11:05:53 -0400

    agregar apuntes primera mitad

---
+++ 00-docentes/sesion-05a/miPrimerCpp/Persona.cpp
@@ -0,0 +1,23 @@
+#include "Persona.h"
+
+// constructor
+Persona::Persona(bool vida) {
+  Persona::estaViva = vida;
+}
+
+// destructor
+Persona::~Persona() {}
+
+// set edad
+void Persona::setEdad(int nuevaEdad) {
+  Persona::edad = nuevaEdad;
+}
+
+// get edad
+int Persona::getEdad() {
+  return Persona::edad;
+}
+
+
+
+
