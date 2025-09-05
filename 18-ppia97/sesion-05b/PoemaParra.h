d8509feff2c7bcc61da2bcfaa175e7077a3a36b5
ppia97 <pia.orellana@mail.udp.cl>
2025-09-05 10:09:46 -0400

    Update PoemaParra.h

--- 18-ppia97/sesion-05b/PoemaParra.h
+++ 18-ppia97/sesion-05b/PoemaParra.h
@@ -1,1 +1,21 @@
+```cpp
+
+#ifndef POEMA_PARRA_H
+#define POEMA_PARRA_H
+
+#include "Poema.h"
+
+class PoemaParra : public Poema
+{
+  public:
+  // constructor
+  PoemaParra();
+
+  // destructor
+  ~PoemaParra();
+    
+};
+
+#endif
+```
 
