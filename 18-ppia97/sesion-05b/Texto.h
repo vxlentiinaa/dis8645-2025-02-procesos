4f895400b0579f9ef2c0022b2b277925ab715d0c
ppia97 <pia.orellana@mail.udp.cl>
2025-09-05 10:14:52 -0400

    Update Texto.h

--- 18-ppia97/sesion-05b/Texto.h
+++ 18-ppia97/sesion-05b/Texto.h
@@ -1,7 +1,12 @@
+EJEMPLO
 ```cpp
 
 #ifndef TEXTO_H
 #define TEXTO_H
+
+#include "Tipo.h"
+#include "Tinta.h"
+#include "Autoria.h"
 
 class Texto {
   // constructor
@@ -11,8 +16,13 @@ class Texto {
   ~Texto();
     
     Tipo tipografia;
+    Tinta tinta;
+    Autoria autora;
+    
     bool izquierdaADerecha = true;
+    float nota = 1.0;
 };
 
 #endif
 ```
+Tinta (tipo) y tinta (nombre de la variable)
