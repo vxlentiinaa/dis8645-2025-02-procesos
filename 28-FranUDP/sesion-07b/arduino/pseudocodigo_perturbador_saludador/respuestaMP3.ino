c8580161ff0b63754b7233385e0f8f8c174af1cf
FranUDP <francisco.stephens@mail.udp.cl>
2025-09-26 12:05:00 -0300

    seudo-arduino

---
+++ 28-FranUDP/sesion-07b/arduino/pseudocodigo_perturbador_saludador/respuestaMP3.ino
@@ -0,0 +1,16 @@
+/*
+
+*/
+
+
+// si la posicion de la persona es menor o igual a la distancia de saludo...
+if posicion =< distanciaSaludo
+
+// reproducir uno de los audios
+hablar ();
+
+// si la persona está invadiendo el espacio personal...
+if else posicion =< distanciaMin
+
+// pedirle que se mueva para atras
+pedirEspacioPersonal ();
