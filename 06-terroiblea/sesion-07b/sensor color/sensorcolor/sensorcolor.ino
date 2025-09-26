2877f95cb192e1a265a5fc24bb9890f000a65a4d
c a t a <catalina.catalan@mail.udp.cl>
2025-09-26 11:53:38 -0300

    Add files via upload

---
+++ 06-terroiblea/sesion-07b/sensor color/sensorcolor/sensorcolor.ino
@@ -0,0 +1,16 @@
+// Sensor Reconocimiento de Color Tcs230 - Tcs3200
+
+
+
+void configurarSensorColor() {
+  //definir patita de entrada solo si es necesario
+ 
+
+}
+
+int valorActualSensorColor = 0
+
+void leerSensorColor() {
+  analogread()
+
+}
