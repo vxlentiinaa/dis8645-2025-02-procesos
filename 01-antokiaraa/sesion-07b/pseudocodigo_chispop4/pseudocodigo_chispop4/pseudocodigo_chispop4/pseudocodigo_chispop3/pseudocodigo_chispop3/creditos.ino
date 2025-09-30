d24fdc679eebbf47600f439f91433c7ad0260e85
antokiaraa <antonella.aguilar@mail.udp.cl>
2025-09-29 21:23:30 -0300

    Delete 01-antokiaraa/sesion-07b/pseudocodigo_chispop4/pseudocodigo_chispop4/pseudocodigo_chispop4/pseudocodigo_chispop3/pseudocodigo_chispop3 directory

--- 01-antokiaraa/sesion-07b/pseudocodigo_chispop4/pseudocodigo_chispop4/pseudocodigo_chispop4/pseudocodigo_chispop3/pseudocodigo_chispop3/creditos.ino
+++
@@ -1,4 +0,0 @@
-// 2025 09 25
-// intento de codigo v0
-// para grupo millaray y sus secuaces
-// parece que necesitan un sensor ultrasonico
--- 01-antokiaraa/sesion-07b/pseudocodigo_chispop4/pseudocodigo_chispop4/pseudocodigo_chispop4/pseudocodigo_chispop3/pseudocodigo_chispop3/sensoresActuadores.ino
+++
@@ -1,6 +0,0 @@
-void configurarSensoresActuadores() {
-  configurarSensorSonido();
-}
-void leerSensores() {
-  // Si más adelante agregamos otros sensores, se leen aquí
-}
