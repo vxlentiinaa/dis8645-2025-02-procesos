97d972da3c4cff1b2f64ec003702ef257f2bac26
aarón montoya-moraga <montoyamoraga@gmail.com>
2025-10-07 11:48:56 -0300

    agregar grupo-04

---
+++ 00-docentes/sesion-09a/grupo-04/SensorUltrasonico.h
@@ -0,0 +1,15 @@
+#ifndef SENSOR_ULTRASONICO_H
+#define SENSOR_ULTRASONICO_H
+
+#include <Arduino.h>
+
+
+class SensorUltrasonico {
+
+public:
+
+  SensorUltrasonico();
+  ~SensorUltrasonico();
+};
+
+#endif
