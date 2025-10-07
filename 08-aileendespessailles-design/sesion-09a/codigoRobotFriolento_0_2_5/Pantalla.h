0fae20835b085d2a3b3eb4634b6b379c21aa8da4
Carla Pino <carla.pino@mail.udp.cl>
2025-10-07 10:59:21 -0300

    Versión con pantalla incluida

---
+++ 08-aileendespessailles-design/sesion-09a/codigoRobotFriolento_0_2_5/Pantalla.h
@@ -0,0 +1,16 @@
+#ifndef PANTALLA_H
+#define PANTALLA_H
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <U8g2lib.h>
+
+class Pantalla{
+  public: 
+  void configurar();
+  void mostrar(); 
+
+};
+
+
+#endif
