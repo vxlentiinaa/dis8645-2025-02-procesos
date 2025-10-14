971eb7a71534a4e454c531fa87b66c4acd3f8e90
félix rodríguez <felix.rodriguez@mail.udp.cl>
2025-10-14 11:14:58 -0300

    codigo-10a

--- 25-felix-rg416/sesion-10a/README.md
+++ 25-felix-rg416/sesion-10a/README.md
@@ -1,1 +1,9 @@
 # sesion-10a
+
+martes 14 de octubre
+
+---
+
+La idea de hoy es hacer que el servo se detenga cuando el sensor de proximidad detecte que hay alguien.
+
+Hice una versión del código para esta clase, llamado codigo-10a, para no dañar el que hicimos la semana pasada.
---
+++ 25-felix-rg416/sesion-10a/codigo-10a/codigo-10a.ino
@@ -0,0 +1,43 @@
+#include "constantes.h"
+#include "SensorProx.h"
+#include "Cuello.h"
+#include "Parpados.h"
+#include "ServoSensores.h"
+#include <Servo.h>
+#include <Arduino.h>
+
+// variable temporal para lecturas
+unsigned int sample;
+
+unsigned long startMillis;
+
+Cuello cuello;
+Parpados parpados;
+ServoSensores sensores;
+
+SensorProx sensorProxIZQ;
+SensorProx sensorProxDER;
+
+bool imprimir = true;
+
+void setup() {
+
+  cuello.configurar();
+  parpados.configurar();
+
+  sensorProxIZQ.configurar();
+
+  // inicia monitor serial
+  Serial.begin(9600);
+}
+
+void loop() {
+ cuello.moverCuello();
+
+  sensorProxIZQ.leer();
+  sensorProxIZQ.determinarPresencia();
+
+  if (sensorProxIZQ.presencia){
+    
+  }
+}
