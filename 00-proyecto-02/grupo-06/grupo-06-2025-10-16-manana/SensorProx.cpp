941cde0051ab7e24d2b98937f8b9eadf21f40302
montoyamoraga <montoyamoraga@gmail.com>
2025-10-16 11:37:34 -0300

    grabar con fecha hoy y clonar version profe

---
+++ 00-proyecto-02/grupo-06/grupo-06-2025-10-16-manana/SensorProx.cpp
@@ -0,0 +1,44 @@
+#include "SensorProx.h"
+
+SensorProx::SensorProx() {}
+
+SensorProx::~SensorProx() {}
+
+void SensorProx::configurar() {
+  pinMode(SensorProx::patitaEcho, INPUT);
+  pinMode(SensorProx::patitaTrigger, OUTPUT);
+}
+
+void SensorProx::leer() {
+
+  digitalWrite(SensorProx::patitaTrigger, LOW);
+  delayMicroseconds(2);
+  digitalWrite(SensorProx::patitaTrigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(SensorProx::patitaTrigger, LOW);
+
+  SensorProx::duracion = pulseIn(
+    SensorProx::patitaEcho,
+    HIGH);
+  SensorProx::distancia = (SensorProx::duracion * 0.0343) / 2;
+}
+
+void SensorProx::determinarPresencia() {
+  if (SensorProx::distancia < 16) {
+    SensorProx::presencia = true;
+  } 
+  else {
+    SensorProx::presencia = false;
+  }
+}
+
+// void SensorProx::nada() {
+//   if (SensorProx::distancia > 15) {
+//   }
+// }
+
+void SensorProx::imprimirEnConsola() {
+  Serial.print("Distancia: ");
+  Serial.println(duracion);
+  delay(100);
+}
