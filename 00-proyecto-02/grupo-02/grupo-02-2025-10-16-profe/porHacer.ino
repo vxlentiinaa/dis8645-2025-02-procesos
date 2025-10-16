f84351602a4e636d3615f9ce537475e3da02766c
montoyamoraga <montoyamoraga@gmail.com>
2025-10-16 10:46:22 -0300

    agregar version clonada desde subcarpeta, muy desordenado

---
+++ 00-proyecto-02/grupo-02/grupo-02-2025-10-16-profe/porHacer.ino
@@ -0,0 +1,31 @@
+
+// NARANJO
+// Información o código bruto de fotogramas de animación Amarillo
+// Amarillo = felicidad 
+// animación ojos felices 
+
+// VERDE
+// Información o código bruto de fotogramas de animación Verde
+// Verde = Loco
+// animación ojos locos
+
+
+// AZUL
+// Información o código bruto de fotogramas de animación Azul
+// Azul = triste 
+// animación ojos tristes 
+// MAS AZUL
+// Da la acción a la respuesta del color Azul
+// activa audio
+// activa animación 
+// funciona en base a if 
+
+
+// ROJO
+// Información o código bruto de fotogramas de animación Rojo
+// Rojo = enojado 
+// animación ojos enojados
+
+// STANDBY
+// Información o código bruto de fotogramas de animación StandBy
+
