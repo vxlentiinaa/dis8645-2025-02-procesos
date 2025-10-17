c831045f98d9720ac45cd4eefaaa5856c73e4036
SebastianSaez1003 <sebastian.saez1@mail.udp.cl>
2025-10-17 08:12:36 -0300

    Delete 00-proyecto-02/grupo-05/codigoRobotFriolento_0_5_0/codigoRobotFriolento_0_5_0 directory

--- 00-proyecto-02/grupo-05/codigoRobotFriolento_0_5_0/codigoRobotFriolento_0_5_0/IntruccionesDeUso.h
+++
@@ -1,5 +0,0 @@
-// Aqui pondremos las instrucciones de como utilizar y hacer funcionar nuestro robot
-
-// Escribir cual modelo especifico de sensores y actuadores hemos ocupado
-
-// escribir los pines a los que deberian ir conectados los componentes
