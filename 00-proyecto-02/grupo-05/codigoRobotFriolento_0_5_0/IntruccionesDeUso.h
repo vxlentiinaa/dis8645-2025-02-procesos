2479330d071947b9c2c100325468fc4fe900d404
SebastianSaez1003 <sebastian.saez1@mail.udp.cl>
2025-10-17 08:15:00 -0300

    Add files via upload

---
+++ 00-proyecto-02/grupo-05/codigoRobotFriolento_0_5_0/IntruccionesDeUso.h
@@ -0,0 +1,15 @@
+// Aqui pondremos las instrucciones de como utilizar y hacer funcionar nuestro robot
+
+// Escribir cual modelo especifico de sensores y actuadores hemos ocupado
+
+// escribir los pines a los que deberian ir conectados los componentes
+
+// Para el display:           Para el Sensor de Tmperatura:       Para el Servomotor:
+// GRD a GND                  GND  a GND                           Cable Amarillo (El de la IZQ) a 9 (PWM~)
+// VCC a 5V                   VCC  a 5V                            Cable Rojo (El de el MEDIO)   a 5V
+// SCL a A5 (Analog in 5)     DATA a 5 (PWM~)                      Cable Negro (El de la DER)    a GND
+// SDA a A4 (Analog in 4)
+
+//GND  a GND  
+//VCC  a 5V  
+//DATA a 5 (PWM~) 
